11c479a2d4dd7d4d175839b2e2c73724ac67617f
Ngoc Thanh Le <2557119@edu.vaniercollege.qc.ca>
2026-08-27 13:30:07 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,93 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+3. Part A
+A1: true, true, true, true
+A2: Because it needs to check every character in the total password length
+A3: It will be out of bound index error
+A4: It would be an error because there is no reference point
+A5: It would not pass the test because there is no lowercase detected
+A6: It would not pass the test because there is no uppercase detected
+ */
+void main() {
+    String password = "Vanier2026";
+
+    boolean upper = false;
+    boolean lower = false;
+    boolean digit = false;
+
+    for (int i = 0; i < password.length(); i++) {
+        char c = password.charAt(i);
+
+        if (Character.isUpperCase(c))
+            upper = true;
+        else if (Character.isLowerCase(c))
+            lower = true;
+        else if (Character.isDigit(c))
+            digit = true;
+    }
+
+    System.out.println(upper);
+    System.out.println(lower);
+    System.out.println(digit);
+    System.out.println(upper && lower && digit);
+    System.out.println((isValidPassword("vANIER2620")));
+}
+/*
+4. Part B
+1: It should be 0
+2:
+3: After incrementing all the tex length
+ */
+public static int countDigits(String text){
+    int count = 0;
+
+    for(int i = 0; i < text.length(); i++){
+           char c = text.charAt(i);
+           if(Character.isDigit(c))
+               count++;
+    }
+    return count;
+}
+/*
+5. Part C
+They are better only for verifying if the condition is fullfilled
+ */
+public static boolean isValidPassword(String password){
+
+    boolean upper = false;
+    boolean lower = false;
+    boolean digit = false;
+
+    for (int i = 0; i < password.length(); i++) {
+        char c = password.charAt(i);
+
+        if (Character.isUpperCase(c))
+            upper = true;
+        else if (Character.isLowerCase(c))
+            lower = true;
+        else if (Character.isDigit(c))
+            digit = true;
+    }
+    boolean min = password.length() >= 8;
+
+    return upper && lower && digit && min;
+}
+/*
+6. Part D
+The <= will cause an out of bound index error
+ */
+public static int countUppercase(String text) {
+    int count = 0;
+
+    for (int i = 0; i <= text.length(); i++) {
+        if (Character.isUpperCase(text.charAt(i)));
+        count++;
+    }
+
+    return count;
+}
+/*
+7. Part E
+ */
+public static String generateCode()
