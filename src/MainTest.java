3225a25d13ad9abcae3882678d88df5907cbd529
Nicolas-distort <2557119@edu.vaniercollege.qc.ca>
2026-09-02 08:31:35 -0400

    Fixes

--- src/MainTest.java
+++ src/MainTest.java
@@ -3,6 +3,7 @@ Part F
 1: The test will pass because the password has at least 8 characters, 1 uppercase, 1 lowercase, and 1 digit
 2: The test will fail because the password has
  */
+import com.sun.tools.javac.Main;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,19 +14,19 @@ public class MainTest {
     }
 
     @Test
-    public void isValidPassword(); {
+    public void isValidPassword() {
         assertEquals(0, StudentAccessValidator.countDigits("Abcdefg1"));
     }
 
     @Test
-    public void countUppercase(); {
-        assertEquals{2 , StudentAccessValidator.countUppercase("V1A2");}
+    public void countUppercase() {
+        assertEquals(2 , Main.countUppercase("V1A2"));
     }
 
     @Test
-    public void generateCode(); {
-        String code = StudentAccessValidator();
+    public void generateCode() {
+        String code = StudentAccessValidator.generateCode();
         assertEquals(4, code.length());
-        "ABCDE".indexOf(character) >= 0;
+        "ABCDE".indexOf(code.charAt()) >= 0;
     }
 }
